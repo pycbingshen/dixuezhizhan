adb5c9eeefe3f2003a18111586891cd4a2aa083f
bnmjtzma <bnmjtzma@123u.com>
2016-04-27 22:03:11 +0800

    仅能单机一次游戏结束按钮

--- TheLastSurvivor/Assets/Script/MainMenu/LoadRoom.cs
+++ TheLastSurvivor/Assets/Script/MainMenu/LoadRoom.cs
@@ -5,8 +5,12 @@ using proto_islandsurvival;
 
 public class LoadRoom : MonoBehaviour 
 {
+    bool flag = false;
+
     void OnClick()
     {
+        if (flag) return;
+        flag = true;
         CMessage mess = new CMessage();
         mess.m_head.m_framenum = Controller.CurrentFrameNum;
         mess.m_head.m_message_id = MessageRegister.Instance().GetID(typeof(CSGameEnd));
