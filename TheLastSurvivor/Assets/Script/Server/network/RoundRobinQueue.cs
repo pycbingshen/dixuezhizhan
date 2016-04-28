2124416810f439c527ca1a9a68bfb41039a58061
glq <glqgeng@JY000395.intranet.123u.com>
2016-04-29 01:28:03 +0800

    删除队列锁

--- TheLastSurvivor/Assets/Script/Server/network/RoundRobinQueue.cs
+++ TheLastSurvivor/Assets/Script/Server/network/RoundRobinQueue.cs
@@ -11,7 +11,7 @@ namespace client
         CMessage[] m_que = new CMessage[QUEUELENGTH];
         int m_head, m_tail;
         public int m_lastframe;
-        private object lockobj = new object();
+        //private object lockobj = new object();
 		public RoundRobinQueue()
         {
 			m_head = m_tail = 0;
@@ -20,11 +20,8 @@ namespace client
 
         public void init()
         {
-            lock (lockobj)
-            {
-                m_head = m_tail = 0;
-                m_lastframe = -1;
-            }
+            m_head = m_tail = 0;
+            m_lastframe = -1;
         }
 
 		public bool empty()
@@ -42,21 +39,15 @@ namespace client
         public void pop()
         {
             if (m_tail == m_head) return;
-            lock (lockobj)
-            {
-                m_head = (m_head + 1) % QUEUELENGTH;
-            }
+            m_head = (m_head + 1) % QUEUELENGTH;
         }
 
         public void push(CMessage x)
         {
             System.Diagnostics.Debug.Assert((m_tail + 1) % QUEUELENGTH != m_head);
-            lock (lockobj)
-            {
-                m_que[m_tail] = x;
-                m_tail = (m_tail + 1) % QUEUELENGTH;
-                m_lastframe = Math.Max(m_lastframe, x.m_head.m_framenum);
-            }
+            m_que[m_tail] = x;
+            m_tail = (m_tail + 1) % QUEUELENGTH;
+            m_lastframe = Math.Max(m_lastframe, x.m_head.m_framenum);
         }
         public int GetLastFrame()
         {
