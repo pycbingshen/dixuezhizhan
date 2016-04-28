0acd3ed730ba6854f823e0d6d0ec150388d33fc1
glq <glqgeng@JY000395.intranet.123u.com>
2016-04-29 02:20:16 +0800

    队列锁还是得加回来

--- TheLastSurvivor/Assets/Script/Server/network/RoundRobinQueue.cs
+++ TheLastSurvivor/Assets/Script/Server/network/RoundRobinQueue.cs
@@ -11,43 +11,52 @@ namespace client
         CMessage[] m_que = new CMessage[QUEUELENGTH];
         int m_head, m_tail;
         public int m_lastframe;
-        //private object lockobj = new object();
-		public RoundRobinQueue()
-        {
-			m_head = m_tail = 0;
-            m_lastframe = -1;
-        }
-
-        public void init()
+        private object lockobj = new object();
+        public RoundRobinQueue()
         {
             m_head = m_tail = 0;
             m_lastframe = -1;
         }
 
-		public bool empty()
+        public void init()
         {
-			if(m_head == m_tail) return true;
-			return false;
+            lock (lockobj)
+            {
+                m_head = m_tail = 0;
+                m_lastframe = -1;
+            }
         }
 
- 		public CMessage front()
+        public bool empty()
         {
-			if(m_tail == m_head) return null;
-			return m_que[m_head];
+            if (m_head == m_tail) return true;
+            return false;
+        }
+
+        public CMessage front()
+        {
+            if (m_tail == m_head) return null;
+            return m_que[m_head];
         }
 
         public void pop()
         {
             if (m_tail == m_head) return;
-            m_head = (m_head + 1) % QUEUELENGTH;
+            lock (lockobj)
+            {
+                m_head = (m_head + 1) % QUEUELENGTH;
+            }
         }
 
         public void push(CMessage x)
         {
             System.Diagnostics.Debug.Assert((m_tail + 1) % QUEUELENGTH != m_head);
-            m_que[m_tail] = x;
-            m_tail = (m_tail + 1) % QUEUELENGTH;
-            m_lastframe = Math.Max(m_lastframe, x.m_head.m_framenum);
+            lock (lockobj)
+            {
+                m_que[m_tail] = x;
+                m_tail = (m_tail + 1) % QUEUELENGTH;
+                m_lastframe = Math.Max(m_lastframe, x.m_head.m_framenum);
+            }
         }
         public int GetLastFrame()
         {
