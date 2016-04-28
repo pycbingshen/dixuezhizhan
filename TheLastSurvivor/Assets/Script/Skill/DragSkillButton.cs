786a9038902c49624236bab5b43e7d1fcf0cca13
unownpang <unownpang@123u.com>
2016-04-28 22:29:51 +0800

    BUG：修改默认手里剑方向

--- TheLastSurvivor/Assets/Script/Skill/DragSkillButton.cs
+++ TheLastSurvivor/Assets/Script/Skill/DragSkillButton.cs
@@ -46,6 +46,19 @@ public class DragSkillButton : ButtonBase
         else
         {
 //            m_skill.CreatSkillEffect(SkillType.feibiao, transform.localPosition);
+            if(skillName == SkillType.feibiao && Vector3.SqrMagnitude(transform.position) < 100f)
+            {
+                CMessage mess2 = new CMessage();
+                mess2.m_head.m_framenum = Controller.CurrentFrameNum;
+                mess2.m_head.m_message_id = MessageRegister.Instance().GetID(typeof(CSSkill));
+                CSSkill proto2 = new CSSkill();
+                GameObject go3 = GameObject.Find("Player/"+GeneralData.myID.ToString());
+                proto2.pos_x = Mathf.Cos((-go3.transform.eulerAngles.y+90) * Mathf.Deg2Rad);
+                proto2.pos_z = Mathf.Sin((-go3.transform.eulerAngles.y+90) * Mathf.Deg2Rad);
+                proto2.skill_id = (int) skillName;
+                mess2.m_proto = proto2;
+                program.SendQueue.push(mess2);
+            }
             CMessage mess = new CMessage();
             mess.m_head.m_framenum = Controller.CurrentFrameNum;
             mess.m_head.m_message_id = MessageRegister.Instance().GetID(typeof(CSSkill));
